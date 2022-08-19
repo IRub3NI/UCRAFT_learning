95fa994ec56e62dbf2a5acdd37206c5c0945d24f
IRub3NI <ruben.torosyan93@gmail.com>
2022-08-19 13:34:04 +0400

    make Bullet class more classic!

--- bullet.mjs
+++ bullet.mjs
@@ -1,29 +1,42 @@
 export class Bullet {
   bulletWidth = 20;
   bulletHeight = 20;
+  bulletSpeed = 1;
 
   constructor(playerHeight, player) {
-    const bullet = document.createElement("div");
-    bullet.style.width = this.bulletWidth + "px";
-    bullet.style.height = this.bulletHeight + "px";
-    bullet.style.backgroundColor = "blue";
-    bullet.style.position = "fixed";
-    bullet.style.top = parseInt(player.style.top) - this.bulletWidth + "px";
-    bullet.style.left =
-      parseInt(player.style.left) +
-      playerHeight / 2 -
-      this.bulletHeight / 2 +
-      "px";
-    document.body.appendChild(bullet);
+    this.bulletY = parseInt(player.style.top) - this.bulletWidth;
+    this.bulletX =
+      parseInt(player.style.left) + playerHeight / 2 - this.bulletHeight / 2;
 
-    const intervalRef = setInterval(function () {
-      const newTop = parseInt(bullet.style.top) - 1;
-      bullet.style.top = newTop + "px";
+    const intervalRef = setInterval(() => {
+      this.bulletY = this.bulletY - this.bulletSpeed;
+      this.moveBullet();
 
-      if (newTop + this.bulletHeight < 0) {
-        bullet.remove();
+      if (this.bulletY + this.bulletHeight < 0) {
+        this.removeBullet();
         clearInterval(intervalRef);
       }
     }, 1);
+
+    this.createAndAppendBulletDom();
+  }
+
+  createAndAppendBulletDom() {
+    this.bulletDom = document.createElement("div");
+    this.bulletDom.style.width = this.bulletWidth + "px";
+    this.bulletDom.style.height = this.bulletHeight + "px";
+    this.bulletDom.style.backgroundColor = "blue";
+    this.bulletDom.style.position = "fixed";
+    this.bulletDom.style.top = this.bulletY + "px";
+    this.bulletDom.style.left = this.bulletX + "px";
+    document.body.appendChild(this.bulletDom);
+  }
+
+  moveBullet() {
+    this.bulletDom.style.top = this.bulletY + "px";
+  }
+
+  removeBullet() {
+    this.bulletDom.remove();
   }
 }
